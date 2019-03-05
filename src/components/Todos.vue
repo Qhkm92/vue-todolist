7c49fc40605378d0133411b9c8f198747bed8a88
Qhkm92 <replace-all>
2019-03-05 21:15:25 +0900

    Add header layout

---
+++ src/components/Todos.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div v-bind:key="todo.id" v-for="todo in todos">
+            <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)"/>
+        </div>
+    </div>
+</template>
+
+
+<script>
+import TodoItem from './TodoItem'
+
+export default {
+    name: 'Todos', //export default Todos so that App can use
+    components: {
+        TodoItem //children component
+    },
+    props: ["todos"] //props that was passed from app
+}
+</script>
