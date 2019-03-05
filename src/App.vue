b7625311a2d22125f1b3583d278d740779a79adb
Qhkm92 <replace-all>
2019-03-05 20:07:04 +0900

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app"> 
+    <div v-for="todo in todos">
+      <li>{{todo.title}}</li>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: 'To do one',
+          completed: false
+        },
+        {
+          id: 2,
+          title: 'To do two',
+          completed: true
+        },
+        {
+          id: 3,
+          title: 'To do three',
+          completed: false
+        },
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
 }
 </style>
