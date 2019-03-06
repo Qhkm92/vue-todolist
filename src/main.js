eda7bd497b591e3ee5798a3cc2d61e3341bbf7c9
Qhkm92 <replace-all>
2019-03-06 13:48:13 +0900

    Add vue router and configuration file

--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
 import App from './App.vue'
+import Home from './components/Home.vue'
+import About from './components/About.vue'
 
 Vue.config.productionTip = false
 
+const router = new VueRouter({
+    routes: 
+    [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: About
+        }
+    ],
+    mode: 'history'
+})
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
